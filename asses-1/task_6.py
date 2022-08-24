b0843f0e65c14ee9815da711348a060837c1dd5e
Ekaterina <semyanihina@yandex.ru>
2022-08-24 23:50:36 +0300

    different lits' length

--- asses-1/task_6.py
+++ asses-1/task_6.py
@@ -1,7 +1,8 @@
 list1 = [3, 5, 7, 'jo']
-list2 = [6, 'jo', 3, 9]
+list2 = [6, 'jo', 3, 9, 5, 7]
 list3 = []
-
+if len(list1) < len(list2):
+    list1, list2 = list2, list1
 for i in list1:
     for j in list2:
         if i == j:
