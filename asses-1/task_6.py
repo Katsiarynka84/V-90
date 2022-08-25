1c9d90030ad83bf5b36df8e3608578ed405e2183
Ekaterina <semyanihina@yandex.ru>
2022-08-25 22:10:15 +0300

    break added

--- asses-1/task_6.py
+++ asses-1/task_6.py
@@ -1,12 +1,13 @@
 list1 = [3, 5, 7, 'jo']
 list2 = [6, 'jo', 3, 9, 5, 7]
 list3 = []
-if len(list1) < len(list2):
+if len(list1) > len(list2):
     list1, list2 = list2, list1
 for i in list1:
     for j in list2:
         if i == j:
             list3.append(i)
+            break
 print(list3)
 
 
