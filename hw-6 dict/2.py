63fd9968d3f740f7b5072f5fde2f72214e5918b1
Ekaterina <semyanihina@yandex.ru>
2022-09-12 15:18:24 +0300

    optimization

--- hw-6 dict/2.py
+++ hw-6 dict/2.py
@@ -1,10 +1,5 @@
 dict1 = {'a': 300, 5: 25.52, 'd': 4}
 dict2 = {5: 2, 'a': 3, 'c': 17}
-if dict1 == dict2:
-    dict3 = {i: dict1[i] * dict2[i] for i in dict2}
-    print(dict3)
-else:
-    dict3 = {i: dict1[i] * dict2[i] for i in dict2 if i in dict1}
-    dict2.update(dict3)
-    dict1.update(dict2)
-    print(dict1)
+dict3 = dict1 | dict2
+dict3 |= {key: dict1[key] * dict2[key] for key in dict2 if key in dict1}
+print(dict3)
