1ba72231606a6114559df438be9d6f7055f108d9
Ekaterina <semyanihina@yandex.ru>
2022-09-16 19:01:01 +0300

    exceptions calcul

--- hw-1-calculator/calculator.py
+++ hw-1-calculator/calculator.py
@@ -1,13 +1,20 @@
 
-
-a = int(input())
-b = int(input())
-c = input()
-if c == '/':
-    if b == 0:
-        print('На ноль делить нельзя')
+while True:
+    try:
+        a = int(input('Введите первое число: '))
+        b = int(input('Введите второе число: '))
+        c = input('Введите оператор: ')
+        if c not in ('*/+-%!><='):
+            print('Некорректный оператор')
+    except ValueError:
+        print('Вы ввели не число. Попробуйте еще раз.')
     else:
+        break
+if c == '/':
+    try:
         print(a / b)
+    except ZeroDivisionError:
+        print('На ноль делить нельзя')
 elif c == '+':
     print(a + b)
 elif c == '-':
