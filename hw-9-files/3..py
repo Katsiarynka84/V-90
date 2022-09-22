06bff2a7c22ce986331a77f238995dcfbe3a6a33
Ekaterina <semyanihina@yandex.ru>
2022-09-22 22:18:38 +0300

    3. count changed

--- hw-9-files/3..py
+++ hw-9-files/3..py
@@ -1,9 +1,15 @@
 with open('file.txt') as my_file:
-    print(f'Количество строк в файле: {len(my_file.readlines())}')
+    line_counter = 0
+    lines = my_file.readlines()
+    for i in lines:
+        if len(i) > 1:
+            line_counter += 1
+    print(f'Количество  заполненных строк в файле: {line_counter}')
+    print(f'Количество пустых строк в файле: {len(lines)-line_counter}')
 
     my_file.seek(0)
     text = my_file.read()
-    #print(text)
+    # print(text)
     my_list = []
     word = ''
     counter = 0
