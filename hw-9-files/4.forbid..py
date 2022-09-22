75ebca24b29fdbef729b29c2c0db892ebb0dabb2
Ekaterina <semyanihina@yandex.ru>
2022-09-23 00:17:31 +0300

    4.forbidden

---
+++ hw-9-files/4.forbid..py
@@ -0,0 +1,19 @@
+with open('forbidden.txt') as forb_list:
+    my_list = [i for i in forb_list.read().split()]
+
+doc = 'file.txt'
+
+with open(doc) as file_1:
+    text = file_1.readlines()
+    new_text = []
+
+    for i in range(len(text)):
+        s = text[i]
+
+        for word in my_list:
+            if word in s:
+                number = text[i].count(word)
+                s = s.replace(word, '*' * len(word), number)
+        new_text.append(s.rstrip(), )
+
+print(*new_text, sep='\n')
