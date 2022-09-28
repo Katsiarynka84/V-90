a57f9cfab5ce9ebd5f2a32bb35ba3f14703cb5a1
Ekaterina <semyanihina@yandex.ru>
2022-09-28 23:10:39 +0300

    task1

---
+++ asses-2/1.py
@@ -0,0 +1,10 @@
+s = (5, 6, '1', '2', '2', '3', '3', '3', '4', '4')
+# try:
+#     ss = [int(i) for i in s]
+#     print(tuple(sorted(ss)))
+
+try:
+    print(*sorted(list(map(int,list(s)))))
+
+except ValueError:
+    print('ошибка', s)
