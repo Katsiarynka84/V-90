4f2a2ceeb9dadae7451e5e34b540201ac2862a13
Ekaterina <semyanihina@yandex.ru>
2022-09-30 09:57:02 +0300

    task10 optim

--- asses-2/10.py
+++ asses-2/10.py
@@ -1,8 +1,11 @@
-text = open('file.txt')
-print("Количество строк в тексте:", len(text.readlines()))
-lat = 0
-text.seek(0)
-for i in text.read():
-    if 91 > ord(i.upper()) > 64:
-        lat += 1
-print('Количество латинских букв в тексте:', lat)
+with open('file.txt') as text:
+    lat = 0
+    lines = 0
+    for line in text.readlines():
+        lines += 1
+        for symb in line:
+            if 91 > ord(symb.upper()) > 64:
+                lat += 1
+    print('Количество латинских букв в тексте:', lat)
+    print("Количество строк в тексте:", lines)
+
