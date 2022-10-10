b76a587094f7a8acef095cc963dd3747f554c2ea
Ekaterina <semyanihina@yandex.ru>
2022-10-10 08:35:46 +0300

    pangranmmA

--- hw-10 func/3.py
+++ hw-10 func/3.py
@@ -1,7 +1,7 @@
 def main():
     def pangr(s):
         import string
-        return 'Строка-панграмм!' if set(string.ascii_lowercase).issubset(set(s.lower())) else 'Это не панграмм!'
+        return 'Строка-панграммa!' if set(string.ascii_lowercase).issubset(set(s.lower())) else 'Это не панграммa!'
     print(pangr(input(f'Введите строку: \n')))
 
 
