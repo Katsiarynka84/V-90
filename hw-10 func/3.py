31cb7c61250437f712d0d29012d4da09b78ab5d4
Ekaterina <semyanihina@yandex.ru>
2022-10-10 09:09:17 +0300

    3.1

--- hw-10 func/3.py
+++ hw-10 func/3.py
@@ -1,8 +1,19 @@
+import random
+import string
+
+
 def main():
     def pangr(s):
         import string
-        return 'Строка-панграммa!' if set(string.ascii_lowercase).issubset(set(s.lower())) else 'Это не панграммa!'
-    print(pangr(input(f'Введите строку: \n')))
+        return f'"{s}" Строка-панграммa!' if set(string.ascii_lowercase).issubset(set(s.lower())) \
+            else f'Это "{s}" не панграммa!'
+
+    a = list(string.ascii_letters)
+    random.shuffle(a)
+    print(pangr(''.join(a)))
+    b = ''.join(random.sample(string.ascii_letters+string.digits, 40))
+    print(pangr(b))
+    print(pangr((input(f'Введите строку: \n'))))
 
 
 if __name__ == '__main__':
