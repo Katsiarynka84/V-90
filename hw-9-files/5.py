5dbb3c19f8d16d163843d18fac1dc0506a9118c9
Ekaterina <semyanihina@yandex.ru>
2022-10-12 16:00:09 +0300

    better

--- hw-9-files/5.py
+++ hw-9-files/5.py
@@ -2,5 +2,19 @@ numbers = [i for i in input('Введите числа через пробел: 
 with open ('number_list.txt', 'w') as doc:
     for i in numbers:
         print(i, file=doc)
-with open('number_list.txt') as doc:
-    print(doc.read())
+
+try:
+    with open('number_list.txt', 'r+') as doc:
+        print(doc.read())
+        doc.seek(0)
+        mult = 1
+        summa = 0
+        for i in doc.readlines():
+            try:
+                summa += float(i)
+                mult *= float(i)
+            except ValueError as d:
+                print('Все введенные значения должны быть числами', d)
+        print(summa, mult, sep='\n', file=doc)
+except Exception as e:
+    print('ошибка при чтении файла', e)
