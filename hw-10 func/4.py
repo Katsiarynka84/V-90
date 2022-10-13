5f68240749eb813bbe923ea7631e49dc53440d80
Ekaterina <semyanihina@yandex.ru>
2022-10-13 09:37:48 +0300

    doc,annot.

--- hw-10 func/4.py
+++ hw-10 func/4.py
@@ -1,7 +1,8 @@
 def main():
     mylist = [3, 0, 5, True, 'kjhg']
 
-    def to_set(a):
+    def to_set(a: list):
+        """Преобразует список в множество, игнорируя нехэшируемые объекты."""
         return set(filter(lambda x: isinstance(x, (int, str, tuple, bool)), a))
 
     print(to_set(mylist))
