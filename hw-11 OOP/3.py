d35c5bb99abbcebc5ce9cc77a684ced47224aebe
Ekaterina <semyanihina@yandex.ru>
2022-10-26 12:49:00 +0300

    property

--- hw-11 OOP/3.py
+++ hw-11 OOP/3.py
@@ -1,11 +1,11 @@
 class Car:
     def __init__(self, car_type=None, year=None, color=None):
         if car_type:
-            self.type = car_type
+            self._type = car_type
         if year:
-            self.year = year
+            self._year = year
         if color:
-            self.color = color
+            self._color = color
 
     @staticmethod
     def turn_on():
@@ -15,28 +15,37 @@ class Car:
     def turn_off():
         print('Автомобиль заглушен')
 
+    ##Поле color##
+    def get_color(self):
+        return self._color
+
     def set_color(self, color):
-        self.color = color
+        self._color = color
+
+    car_color = property(get_color, set_color)
+
+
 
     def set_year(self, year):
         if isinstance(year,int) and 1900 < year < 2022:
-            self.year = year
+            self._year = year
         else:
             print('Проверьте данные')
 
     def set_type(self, car_type):
-        self.type = car_type
+        self._type = car_type
 
 
 ferrari = Car()
 bugatti = Car('sedan', 1955, 'green')
-lamborgini = Car()
+lamborghini = Car()
 
-lamborgini.turn_on()
+lamborghini.turn_on()
 bugatti.set_type('cabriolet')
-lamborgini.set_year(2023)
-ferrari.set_color('blue')
+lamborghini.set_year(2023)
+ferrari.car_color = 'blue'
+print(ferrari.car_color)
 
 print(ferrari.__dict__)
 print(bugatti.__dict__)
-print(lamborgini.__dict__)
+print(lamborghini.__dict__)
