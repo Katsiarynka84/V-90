573b607bd91c49b22b2001d9626210e405d5979e
Ekaterina <semyanihina@yandex.ru>
2022-10-26 12:04:12 +0300

    с дескриптором

--- hw-11 OOP/2.py
+++ hw-11 OOP/2.py
@@ -1,17 +1,30 @@
-class Math:
 
-    def __init__(self, a, b):
-        if type(a) in (int, float) and type(b) in (int, float):
-            self.a = a
-            self.b = b
+class NumberField:
+
+    def __set_name__(self, owner, name):
+        self.name = name
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+    def __set__(self, instance, value):
+        if type(value) in (int, float):
+            instance.__dict__[self.name] = value
         else:
             raise ValueError('Некорректные данные')
 
+
+
+class SimpleMathExpression:
+    a = NumberField()
+    b = NumberField()
+
+    def __init__(self, a, b):
+            self.a = a
+            self.b = b
+
     def division(self):
-        try:
-            return round((self.a / self.b), 3)
-        except ZeroDivisionError:
-            return 'Ошибка деления на ноль'
+        if self.b ==  0:
+            raise ZeroDivisionError
+        return round((self.a / self.b), 3)
 
     def multiplication(self):
         return self.a * self.b
@@ -23,5 +36,5 @@ class Math:
         return self.a - self.b
 
 
-obj = Math(6.9, 17)
+obj = SimpleMathExpression(6.9, 0)
 print(obj.division())
