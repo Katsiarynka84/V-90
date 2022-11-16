70abeb483d1f94774910abc9c6ac00d00d071a91
Ekaterina <semyanihina@yandex.ru>
2022-11-16 11:21:28 +0300

    decimal?

--- hw-12_modul/1_.py
+++ hw-12_modul/1_.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 s = '2.45 2.69 2.45 3.45 2.00 0.04 7.25'
+# print(*sorted(s.split()))
 my_list = [Decimal(i) for i in s.split()]
 print(sum(my_list))
 print(*[float(i) for i in sorted(my_list)])
