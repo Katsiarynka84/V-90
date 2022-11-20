4b0afbe7a800e8ae42b6ef77e34fdadcf4a1c41e
Ekaterina <semyanihina@yandex.ru>
2022-11-20 23:20:18 +0300

    bus

---
+++ asses-3/_5_.py
@@ -0,0 +1,18 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+
+    def fare(self):
+        return self.capacity * 100
+
+
+class Bus(Vehicle):
+    def fare(self):
+        #return Vehicle.fare(self) + Vehicle.fare(self)/100 * 10
+        return self.capacity * 100 + self.capacity * 10
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
